db06013e7554f8c67e16dc6ff68b30cecf34e761
daver987 <david@drobertson.pro>
2023-02-16 20:42:18 -0500

    Feature: use prisma and make a new priciing engine

---
+++ components/Input/InputPlacesAutocompleteDark.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import { Loader } from '@googlemaps/js-api-loader'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text',
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    required: false,
+    default: 'input-el',
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  errorMessage: {
+    type: String,
+    default: 'Required -*',
+  },
+  placeholder: {
+    type: String,
+    default: 'Enter Location',
+  },
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  showError: {
+    required: false,
+    type: Boolean,
+    default: false,
+  },
+})
+function formatAddress(name: string | undefined, address: string | undefined) {
+  return address.includes(name) ? address : `${name}, ${address}`
+}
+
+const mapsApiKey = useRuntimeConfig().public.GOOGLE_MAPS_API_KEY
+const autocomplete = ref<google.maps.places.Autocomplete | null>(null)
+const inputField = ref<HTMLInputElement | null>(null)
+const place = ref<google.maps.places.PlaceResult | null>(null)
+
+const loader = new Loader({
+  apiKey: mapsApiKey,
+  version: 'weekly',
+  libraries: ['places'],
+})
+
+const initAutocomplete = async () => {
+  const inputEl = document.getElementById(props.name) as HTMLInputElement
+
+  await loader.load().then(() => {
+    autocomplete.value = new google.maps.places.Autocomplete(
+      inputEl!,
+
+      {
+        componentRestrictions: { country: ['us', 'ca'] },
+        fields: ['place_id', 'formatted_address', 'name', 'types'],
+      }
+    )
+    autocomplete.value.addListener('place_changed', getAutocompleteComponents)
+  })
+}
+
+const getAutocompleteComponents = () => {
+  place.value = autocomplete.value!.getPlace()
+  const { formatted_address, name } = place.value
+  modelValue.value = formatAddress(name, formatted_address)
+  emit('change', place.value)
+}
+
+const emit = defineEmits(['change'])
+
+onMounted(async () => {
+  await initAutocomplete()
+})
+const modelValue = ref<string | undefined>('')
+</script>
+
+<template>
+  <n-input
+    :input-props="{
+      id: name,
+      type: 'text',
+    }"
+    :placeholder="placeholder"
+    v-model:value="modelValue"
+  />
+</template>
---
+++ components/QuoteFormTwo.vue
@@ -0,0 +1,507 @@
+<script setup lang="ts">
+import { FormInst, useMessage, FormItemRule } from 'naive-ui'
+import { VueTelInput } from 'vue-tel-input'
+import { VehicleType } from '~/server/api/vehicles.get'
+import { ServiceType } from '~/server/api/services.get'
+import { useGtm } from '@gtm-support/vue-gtm'
+import { z } from 'zod'
+
+const route = useRoute()
+const gtmValues = route.query
+
+const gtm = useGtm()
+
+function triggerEvent() {
+  gtm?.trackEvent({
+    event: 'submitQuote',
+    category: 'Request quote',
+    action: 'click',
+    label: 'Request Quote',
+    value: 1,
+    noninteraction: false,
+  })
+}
+
+const inputOptions = ref({
+  id: 'phone-number',
+  required: true,
+  showDialCode: true,
+  name: 'phone_number',
+  type: 'tel',
+  ariaDescribedby: 'name',
+  ariaLabeledBy: 'placeholder',
+  placeholder: 'Enter Phone',
+  autoFocus: true,
+})
+const dropdownOptions = ref({
+  showDialCodeInSelection: false,
+  showFlags: true,
+  showSearchBox: true,
+  showDialCodeInList: true,
+})
+
+const buildHoursRequiredOptions = () => {
+  const options = []
+  for (let i = 2; i <= 12; i++) {
+    options.push({
+      label: `${i} hours`,
+      value: i,
+    })
+  }
+  return options
+}
+
+const hourlyOptions = buildHoursRequiredOptions()
+
+const formRef = ref<FormInst | null>(null)
+
+const message = useMessage()
+
+const formValue = ref({
+  first_name: null,
+  last_name: null,
+  email_address: null,
+  phone_number: null,
+  conversion: {
+    ...gtmValues,
+  },
+  origin: {},
+  destination: {},
+  pickup_date: null,
+  pickup_time: null,
+  return_date: null,
+  return_time: null,
+  selected_hours: null,
+  selected_passengers: null,
+  is_hourly: computed(() => {
+    return formValue.value.selected_service_type_value === 4
+  }),
+  selected_vehicle_type_value: null,
+  selected_service_type_value: null,
+  is_round_trip: false,
+}) as any
+
+const rules = {
+  pickup_date: {
+    type: 'number',
+    required: true,
+    message: 'Please enter a pickup date',
+    trigger: ['blur'],
+  },
+  pickup_time: {
+    type: 'number',
+    required: true,
+    message: 'Please enter a pickup time',
+    trigger: 'blur',
+  },
+  return_date: {
+    type: 'number',
+    required: true,
+    message: 'Please enter a drop off date',
+    trigger: 'blur',
+  },
+  return_time: {
+    type: 'number',
+    required: true,
+    message: 'Please enter a drop off time',
+    trigger: 'blur',
+  },
+  selected_hours: {
+    type: 'number',
+    required: false,
+    message: 'Please enter in the amount of hours',
+    trigger: ['blur', 'change'],
+  },
+  selected_passengers: {
+    type: 'number',
+    message: 'Passengers is required',
+    trigger: ['blur', 'change'],
+    required: true,
+  },
+  selected_vehicle_type_value: {
+    type: 'number',
+    trigger: ['blur', 'change'],
+    required: true,
+    message: 'Please select a vehicle type',
+  },
+  selected_service_type_value: {
+    type: 'number',
+    message: 'Please select a service type',
+    trigger: ['blur', 'change'],
+    required: true,
+  },
+  first_name: {
+    required: true,
+    message: 'First name is required',
+    trigger: 'blur',
+  },
+  last_name: {
+    required: true,
+    message: 'Last name is required',
+    trigger: 'blur',
+  },
+  email_address: {
+    required: true,
+    message: 'Email is required',
+    trigger: 'blur',
+  },
+  phone_number: {
+    required: true,
+    message: 'Phone number is required',
+    trigger: 'blur',
+  },
+}
+
+const { data: serviceTypeOptions } = await useFetch<ServiceType>(
+  '/api/services'
+)
+
+const { data: vehicleTypeOptions } = await useFetch<VehicleType>(
+  '/api/vehicles'
+)
+
+const maxPassengers = computed<number>(() => {
+  const vehicleType = vehicleTypeOptions.value!.find(
+    (type) => type.value === formValue.value.selected_vehicle_type_value
+  )
+  formValue.value.selected_passengers = null
+  return vehicleType ? vehicleType.max_passengers : 3
+})
+
+const buildPassengerOptions = (numPassengers: number) => {
+  const options = [
+    {
+      label: '1 Passenger',
+      value: 1,
+      isDisabled: false,
+    },
+  ]
+  for (let i = 2; i <= numPassengers; i++) {
+    options.push({
+      label: `${i} Passengers`,
+      value: i,
+      isDisabled: false,
+    })
+  }
+  return options
+}
+
+const handleChangeOrigin = (evt: any) => {
+  formValue.value.origin = evt
+}
+
+const handleChangeDestination = (evt: any) => {
+  formValue.value.destination = evt
+}
+
+const handleValidateClick = (e: MouseEvent) => {
+  e.preventDefault()
+  formRef.value?.validate((errors) => {
+    if (errors) {
+      console.log(errors)
+      message.error('Invalid')
+    } else {
+      message.success('Valid')
+    }
+  })
+}
+
+const loading = ref(false)
+
+const placeSechema = z
+  .object({
+    place_id: z.string(),
+    formatted_address: z.string(),
+    name: z.string(),
+    types: z.array(z.string()),
+  })
+  .strip()
+
+const tripSchema = z
+  .object({
+    distance_value: z.number(),
+    distance_text: z.string(),
+    duration_value: z.number(),
+    duration_text: z.string(),
+  })
+  .strip()
+
+async function onSubmit() {
+  try {
+    loading.value = true
+    const { data: directions } = await useFetch('/api/directions', {
+      method: 'GET',
+      query: {
+        origin: formValue.value.origin.place_id,
+        destination: formValue.value.destination.place_id,
+      },
+    })
+    const { start_lat, start_lng, end_lat, end_lng } = directions.value as any
+    const origin = placeSechema.parse(formValue.value.origin)
+    const destination = placeSechema.parse(formValue.value.destination)
+    const trip = tripSchema.parse(directions.value)
+
+    formValue.value = {
+      ...formValue.value,
+      origin: {
+        ...origin,
+        lat: start_lat,
+        lng: start_lng,
+      },
+      destination: {
+        ...destination,
+        lat: end_lat,
+        lng: end_lng,
+      },
+      ...trip,
+    }
+
+    console.log(formValue.value)
+    const { data: contactData } = await useFetch('/api/contact', {
+      method: 'POST',
+      body: formValue.value,
+    })
+    console.log('Contact data is:', contactData.value)
+
+    setTimeout(async () => {
+      // triggerEvent()
+      loading.value = false
+      // await navigateTo('/quoted')
+    }, 500)
+  } catch (e) {
+    setTimeout(() => {
+      loading.value = false
+      console.log('error')
+    }, 500)
+  }
+}
+
+const { data: contact } = await useFetch('/api/contact', {
+  method: 'GET',
+})
+console.log(contact.value)
+</script>
+
+<template>
+  <n-grid :cols="2">
+    <n-grid-item :span="1">
+      <n-card>
+        <pre
+          >{{ JSON.stringify(formValue, null, 2) }}
+</pre
+        >
+      </n-card>
+    </n-grid-item>
+    <n-grid-item :span="1">
+      <div
+        class="border-1 rounded border border-white bg-black p-4 sm:mx-auto sm:w-full sm:max-w-lg sm:overflow-hidden sm:rounded-lg"
+      >
+        <h2 class="mt-2 mb-4 text-center text-3xl uppercase text-white">
+          Instant Quote
+        </h2>
+        <n-form
+          ref="formRef"
+          :label-width="80"
+          :model="formValue"
+          :rules="rules"
+        >
+          <n-grid :cols="24" :x-gap="12">
+            <n-form-item-gi
+              :show-label="false"
+              label="Pickup Location"
+              :span="24"
+              path="origin"
+            >
+              <InputPlacesAutocompleteDark
+                @change="handleChangeOrigin"
+                name="origin"
+                placeholder="Enter Pickup Location...."
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :show-label="false"
+              :span="24"
+              label="Dropoff Location"
+              path="destination"
+            >
+              <InputPlacesAutocompleteDark
+                name="destination"
+                @change="handleChangeDestination"
+                placeholder="Enter Dropoff Location...."
+                v-model:value="formValue.destination"
+              />
+            </n-form-item-gi>
+            <n-form-item-gi
+              path="pickup_date"
+              :span="12"
+              :show-label="false"
+              label="Pickup Date"
+            >
+              <n-date-picker
+                v-model:value="formValue.pickup_date"
+                type="date"
+                placeholder="Select Pickup Date..."
+              />
+            </n-form-item-gi>
+            <n-form-item-gi
+              path="pickup_time"
+              :span="12"
+              :show-label="false"
+              label="Pickup Date and Time"
+            >
+              <n-time-picker
+                v-model:value="formValue.pickup_time"
+                format="h:mm a"
+                :clearable="true"
+              />
+              <n-switch v-model:value="formValue.is_round_trip">
+                <template #checked> Round Trip</template>
+                <template #unchecked> One Way</template>
+              </n-switch>
+            </n-form-item-gi>
+          </n-grid>
+          <n-collapse-transition :show="formValue.is_round_trip">
+            <n-grid :cols="24" :x-gap="12">
+              <n-form-item-gi
+                :span="12"
+                :show-label="false"
+                label="Return Date"
+                path="dateTime.return_date"
+              >
+                <n-date-picker
+                  v-model:value="formValue.return_date"
+                  type="date"
+                  placeholder="Select Return Date"
+                />
+              </n-form-item-gi>
+              <n-form-item-gi
+                :span="12"
+                :show-label="false"
+                label="Return Time"
+                path="dateTime.return_time"
+              >
+                <n-time-picker
+                  v-model:value="formValue.return_time"
+                  format="h:mm a"
+                  :clearable="true"
+                />
+              </n-form-item-gi>
+            </n-grid>
+          </n-collapse-transition>
+          <n-grid :cols="24" :x-gap="12">
+            <n-form-item-gi
+              :span="12"
+              :show-label="false"
+              label="Service Type"
+              path="selected_service_type_value"
+            >
+              <n-select
+                v-model:value="formValue.selected_service_type_value"
+                :options="serviceTypeOptions!"
+                placeholder="Select Service Type..."
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :span="12"
+              :show-label="false"
+              label="Vehicle Type"
+              path="selected_vehicle_type_value"
+            >
+              <n-select
+                v-model:value="formValue.selected_vehicle_type_value"
+                :options="vehicleTypeOptions!"
+                placeholder="Select Vehicle Type..."
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :span="12"
+              :show-label="false"
+              label="Passengers"
+              path="selected_passengers"
+            >
+              <n-select
+                v-model:value="formValue.selected_passengers"
+                :options="buildPassengerOptions(maxPassengers)"
+                placeholder="Select Passengers..."
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :span="12"
+              :show-label="false"
+              label="Hours"
+              path="selected_hours"
+            >
+              <n-select
+                v-model:value="formValue.selected_hours"
+                :options="hourlyOptions"
+                placeholder="For Hourly Service..."
+                :disabled="!formValue.is_hourly"
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :show-label="false"
+              label="First Name"
+              path="first_name"
+              :span="12"
+            >
+              <n-input
+                v-model:value="formValue.first_name"
+                placeholder="Enter First Name..."
+                :show-label="false"
+                label="Phone Number"
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :span="12"
+              :show-label="false"
+              label="Last Name"
+              path="last_name"
+            >
+              <n-input
+                v-model:value="formValue.last_name"
+                placeholder="Enter Last Name..."
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :span="12"
+              :show-label="false"
+              label="Email Address"
+              path="email_address"
+            >
+              <n-input
+                v-model:value="formValue.email_address"
+                placeholder="Enter Email Address..."
+              />
+            </n-form-item-gi>
+
+            <n-form-item-gi
+              :span="12"
+              path="phone_number"
+              :show-label="false"
+              label="Phone Number"
+            >
+              <VueTelInput
+                v-model="formValue.phone_number"
+                :dropdown-options="dropdownOptions"
+                :input-options="inputOptions"
+                aria-label="phone input"
+                style-classes="rounded border border-gray-300 pr-1 bg-white shadow-sm focus-within:border-brand-600 focus-within:ring-1 focus-within:ring-brand-600"
+              />
+            </n-form-item-gi>
+          </n-grid>
+          <n-space justify="space-between">
+            <n-button @click="onSubmit"> Get Prices and Availability</n-button>
+            <n-button @click="handleValidateClick"> Reset</n-button>
+          </n-space>
+        </n-form>
+      </div>
+    </n-grid-item>
+  </n-grid>
+</template>
